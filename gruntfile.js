5722e82091deb105cca15c252956378e339e4665
Jennifer Schnaible <jennifer@codeatelier.com>
2019-03-22 11:51:55 +0100

    update css
    update css

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,26 @@
 module.exports = function(grunt) {
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
         grunt.initConfig({
             jsdoc: {
                 dist: {
-                    src: ["./app/controller/*.js", "./app/renderer/*.js", "./app/classes/*.js"],
-                    dest: "./doc",
+                    src: [
+                        './app/controller/*.js',
+                        './app/renderer/*.js',
+                        './app/classes/*.js'
+                    ],
+                    dest: './doc',
                     options: {
-                        readme : './README.md'
+                        readme: './README.md'
                     }
                 }
             },
             sass: {
                 options: {
-                    sourceMap: false
+                    sourceMap: true
                 },
                 financer: {
                     files: {
-                        "./themes/css/financer.css": "./themes/financer.scss"
+                        './themes/css/financer.css': './themes/financer.scss'
                     }
                 }
             },
@@ -29,18 +33,18 @@ module.exports = function(grunt) {
                     files: [
                         {
                             expand: true,
-                            cwd: "./themes/css",
-                            src: ["*.css", "!*.min.css"],
-                            dest: "./app/resources/css",
-                            ext: ".min.css"
+                            cwd: './themes/css',
+                            src: ['*.css', '!*.min.css'],
+                            dest: './app/resources/css',
+                            ext: '.min.css'
                         }
                     ]
                 }
             },
             watch: {
                 scripts: {
-                    files: ["./themes/*.scss"],
-                    tasks: ["sass", "cssmin"],
+                    files: ['./themes/*.scss'],
+                    tasks: ['sass', 'cssmin'],
                     options: {
                         spawn: false
                     }
@@ -48,21 +52,21 @@ module.exports = function(grunt) {
             },
             clean: {
                 yourTarget: {
-                    src: ["./app/resources/css/*.css"]
+                    src: ['./app/resources/css/*.css']
                 }
             }
         });
 
-    grunt.loadNpmTasks("grunt-jsdoc");
-    grunt.loadNpmTasks("grunt-sass");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.registerTask("default", ["jsdoc"]);
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['jsdoc']);
 
-    grunt.registerTask("refresh", "Sass to CSS", function() {
-        grunt.log.writeln("Create CSS Files from SASS Soources");
-        grunt.task.run("sass");
-        grunt.task.run("cssmin");
+    grunt.registerTask('refresh', 'Sass to CSS', function() {
+        grunt.log.writeln('Create CSS Files from SASS Soources');
+        grunt.task.run('sass');
+        grunt.task.run('cssmin');
     });
 };
